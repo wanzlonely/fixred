249518b97a278a64fb0ffb621fc3ef5791021375
wanzlonely <wanzlonely04@gmail.com>
2026-01-26 12:03:22 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const CONFIG = {
+    botToken: '8301511202:AAHUSxb-3Jomliqs0O7XpE_JEfjOwNwtSoE',
+    ownerId: '7650101390',
+    ownerUsername: 'walzyexploit',
+    requiredChannel: '@walzyexploit',
+    redisUrl: "https://positive-oarfish-42273.upstash.io",
+    redisToken: "AaUhAAIncDIxZGU1ZmJhYWRhMWU0NjRkYjAxOGUzNmExZTg0ZjkxN3AyNDIyNzM",
+    maxEmails: 20,
+    maxCountPerEmail: 15
+};
